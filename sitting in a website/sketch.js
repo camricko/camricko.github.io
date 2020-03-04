25eecaa86ea6fd482e2d2a43783ee47917b03725
camricko <60410262+camricko@users.noreply.github.com>
2020-03-03 22:49:14 -0600

    Add files via upload

---
+++ sitting in a website/sketch.js
@@ -0,0 +1,22 @@
+// setup(){
+//   createCanvas(100, 100);
+//   background(23, 150, 250);
+// }
+function setup() {
+  createCanvas(400, 400);
+for (i=0; i<20; i++){
+  console.log("uwu owo number" + i);
+}
+}
+
+function draw(){
+  background(220);
+  if (frameCount<500){
+    console.log("aaaaaaahhhh");
+  } else {
+    console.log("done");
+}
+
+let x = 4;
+console.log(x);
+}
